9d311c5e0858a89a3708e223c2aed72808f21e4d
Alastair <alastair05@gmail.com>
2022-03-30 21:10:10 -0700

    Add ajax get, add timeago for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,29 @@
 /* eslint-disable no-undef */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 const createTweetElement = function(tweetData) {
   let $tweet = `
@@ -41,7 +42,7 @@ const createTweetElement = function(tweetData) {
         </div>
         <footer class="tweets-footer">
           <div class="date">
-            <p>Date</p>
+            <p>${timeago.format(tweetData.created_at)}</p>
           </div>
           <div class="icons">
             <i class="fa fa-flag"></i>
@@ -57,19 +58,25 @@ const renderTweets = function(data) {
   for (let tweet of data) {
     const $tweet = createTweetElement(tweet);
     // console.log($tweet);
-    $('#tweets-container').append($tweet);
+    $('#tweets-container').prepend($tweet);
   }
 };
 
 
 $(document).ready(() => {
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax({type: "GET", url: "/tweets"})
+      .then(function(data) {
+        renderTweets(data);
+      });
+  };
+  loadTweets();
 
   $(".textarea").submit(function(event) {
     event.preventDefault();
     const tweetInput = $(this).serialize();
     $.post("/tweets", tweetInput);
-    console.log(tweetInput);
+    // console.log(tweetInput);
   });
 });
 
