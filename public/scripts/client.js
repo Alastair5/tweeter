8fb84e230339540a26bc6256c32fd4b7590c5d12
Alastair <alastair05@gmail.com>
2022-03-31 16:08:19 -0700

    Add jquery hidden error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,5 @@
 /* eslint-disable no-undef */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
 const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -32,6 +7,8 @@ const escape = function(str) {
 };
 
 const createTweetElement = function(tweetData) {
+  $("#empty").hide();
+  $("#exceeds").hide();
   let $tweet = `
     <article class="tweets">
         <header class="tweets-header">
@@ -79,15 +56,19 @@ $(document).ready(() => {
 
   $(".textarea").submit(function(event) {
     event.preventDefault();
+    $("#empty").slideUp();
+    $("#exceeds").slideUp();
     const tweetInput = $(this).serialize();
     const tweetLimit = $("#tweet-text").val().length;
     if (tweetLimit <= 0) {
-      return alert("Please enter a tweet");
+      return $("#empty").slideDown();
     } else if (tweetLimit > 140) {
-      return alert("Please keep your tweet under 140 characters");
+      return $("#exceeds").slideDown();
+    } else {
+      $.post("/tweets", tweetInput);
+      loadTweets();
+      return;
     }
-    $.post("/tweets", tweetInput);
-    loadTweets();
   });
 });
 
